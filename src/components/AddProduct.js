6601ede359d512f96e286dae8de825596cb5cb79
shiba_1234 <shibajisahu121@gmail.com>
2023-04-22 16:12:47 +0530

    Add Product Page implemented

--- src/components/AddProduct.js
+++ src/components/AddProduct.js
@@ -1,9 +1,16 @@
 import React from "react";
+import "../styles/addProduct.css";
 
 function AddProduct() {
   return (
-    <div>
-      <h1>Add Product Page</h1>
+    <div className="add-product-container">
+      <h1>Enter The Product Details</h1>
+      <input type="text" placeholder="Enter Product Name" />
+      <input type="link" placeholder="Paste The Product Image Link" />
+      <input type="number" placeholder="Enter Product Price" />
+      <input type="text" placeholder="Enter Product Rating" />
+      <textarea rows="4" cols="100" placeholder="Description about Product" />
+      <button type="submit">Add Product</button>
     </div>
   );
 }
