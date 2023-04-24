58c69f98cdd514d5b618e5fa2fd59d1f627bab41
shiba_1234 <shibajisahu121@gmail.com>
2023-04-24 12:46:51 +0530

    edit mode UI added

--- src/components/AllProductsPage.js
+++ src/components/AllProductsPage.js
@@ -1,7 +1,5 @@
 import React, { useEffect, useRef } from "react";
 import "../styles/allProduct.css";
-import { Link, useNavigate } from "react-router-dom";
-import StarRating from "./StarRating";
 import { useDispatch, useSelector } from "react-redux";
 import {
   addToCart,
@@ -10,10 +8,10 @@ import {
   selectedProduct,
   sortByPrice,
 } from "../actions/action";
+import ProductList from "./ProductList";
 
 function AllProductsPage() {
   const dispatch = useDispatch();
-  const navigate = useNavigate();
   const ref = useRef();
   const products = useSelector((state) => state.productReducer.productList);
   console.log(products);
@@ -60,106 +58,15 @@ function AllProductsPage() {
           ref.current.style.display = "none";
         }}
       />
-      <div className="product-container">
-        <Link to={`/productDetail/1`}>
-          <img
-            src="https://m.media-amazon.com/images/I/41T493g+UrL._SX307_BO1,204,203,200_.jpg"
-            alt="product-img"
-            // onClick={() => handleSelectProduct(product)}
-          />
-        </Link>
-        <div className="product-left-container">
-          <h1>Book</h1>
-          <div className="rating">
-            Rating : <StarRating starCount="2" />
-            <span>2.0</span>
-          </div>
-          <div className="product-details">
-            Lorem A bestseller since it was originally published in 1937, this
-            hardcover edition of Napoleon Hill`s Think and Grow Rich teaches the
-            famous Andrew Carnegie f
-          </div>
-        </div>
-        <div className="product-right-container">
-          <div className="price">Price: ₹500.00</div>
-          <button className="blue-btn" type="submit">
-            Edit
-          </button>
-          <button
-            className="red-btn"
-            type="submit"
-            // onClick={() => handleDeleteProduct(product)}
-          >
-            Delete
-          </button>
-          <button
-            className="blue-btn"
-            type="submit"
-            // onClick={() => {
-            //   handleSelectProduct(product);
-            //   return navigate(`/productDetail/${product.id}`);
-            // }}
-          >
-            Product Details
-          </button>
-          <button
-            className="trans-btn"
-            type="submit"
-            // onClick={() => addProductToCart(product)}
-          >
-            Add To Cart
-          </button>
-        </div>
-      </div>
       {products.map((product, index) => {
         return (
-          <div key={index} id={product.id} className="product-container">
-            <Link to={`/productDetail/${product.id}`}>
-              <img
-                src={product.image}
-                alt="product-img"
-                onClick={() => handleSelectProduct(product)}
-              />
-            </Link>
-            <div className="product-left-container">
-              <h1>{product.title}</h1>
-              <div className="rating">
-                Rating : <StarRating starCount={product.rating} />
-                <span>{product.rating}.0</span>
-              </div>
-              <div className="product-details">{product.about}</div>
-            </div>
-            <div className="product-right-container">
-              <div className="price">Price: ₹{product.price}.00</div>
-              <button className="blue-btn" type="submit">
-                Edit
-              </button>
-              <button
-                className="red-btn"
-                type="submit"
-                onClick={() => handleDeleteProduct(product)}
-              >
-                Delete
-              </button>
-              <button
-                className="blue-btn"
-                type="submit"
-                onClick={() => {
-                  handleSelectProduct(product);
-                  return navigate(`/productDetail/${product.id}`);
-                }}
-              >
-                Product Details
-              </button>
-              <button
-                className="trans-btn"
-                type="submit"
-                onClick={() => addProductToCart(product)}
-              >
-                Add To Cart
-              </button>
-            </div>
-          </div>
+          <ProductList
+            product={product}
+            key={index}
+            handleDeleteProduct={handleDeleteProduct}
+            addProductToCart={addProductToCart}
+            handleSelectProduct={handleSelectProduct}
+          />
         );
       })}
     </div>
---
+++ src/components/ProductList.js
@@ -0,0 +1,143 @@
+import React, { useState } from "react";
+import StarRating from "./StarRating";
+import { Link, useNavigate } from "react-router-dom";
+
+function ProductList(props) {
+  const [editMode, setEditMode] = useState(false);
+  const [title, setTitle] = useState("");
+  const [price, setPrice] = useState("");
+  const [rating, setRating] = useState("");
+  const [about, setAbout] = useState("");
+  const navigate = useNavigate();
+
+  return (
+    <>
+      {editMode ? (
+        <div
+          key={props.key}
+          id={props.product.id}
+          className="product-container"
+        >
+          <Link to={`/productDetail/${props.product.id}`}>
+            <img src={props.product.image} alt="product-img" />
+          </Link>
+          <div className="product-left-container">
+            <input
+              type="text"
+              value={props.product.title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+            <div className="rating">
+              Rating :
+              <span>
+                <input
+                  type="number"
+                  value={props.product.rating}
+                  onChange={(e) => setRating(e.target.value)}
+                />{" "}
+              </span>
+            </div>
+            <div className="product-details">
+              <textarea
+                rows="6"
+                cols="110"
+                value={props.product.about}
+                onChange={(e) => setAbout(e.target.value)}
+              />
+            </div>
+          </div>
+          <div className="product-right-container">
+            <div className="price">
+              Price:{" "}
+              <input
+                type="number"
+                value={props.product.price}
+                onChange={(e) => setPrice(e.target.value)}
+              />
+            </div>
+            <button className="green-btn" type="submit">
+              Save
+            </button>
+            <button
+              className="red-btn"
+              type="submit"
+              onClick={() => {
+                setEditMode(false);
+              }}
+            >
+              Discard
+            </button>
+            <button
+              className="blue-btn"
+              type="submit"
+              onClick={() => {
+                props.handleSelectProduct(props.product);
+                return navigate(`/productDetail/${props.product.id}`);
+              }}
+            >
+              Product Details
+            </button>
+          </div>
+        </div>
+      ) : (
+        <div
+          key={props.key}
+          id={props.product.id}
+          className="product-container"
+        >
+          <Link to={`/productDetail/${props.product.id}`}>
+            <img
+              src={props.product.image}
+              alt="product-img"
+              onClick={() => props.handleSelectProduct(props.product)}
+            />
+          </Link>
+          <div className="product-left-container">
+            <h1>{props.product.title}</h1>
+            <div className="rating">
+              Rating : <StarRating starCount={props.product.rating} />
+              <span>{props.product.rating}.0</span>
+            </div>
+            <div className="product-details">{props.product.about}</div>
+          </div>
+          <div className="product-right-container">
+            <div className="price">Price: ₹{props.product.price}.00</div>
+            <button
+              className="blue-btn"
+              type="submit"
+              onClick={() => setEditMode(true)}
+            >
+              Edit
+            </button>
+            <button
+              className="red-btn"
+              type="submit"
+              onClick={() => props.handleDeleteProduct(props.product)}
+            >
+              Delete
+            </button>
+            <button
+              className="blue-btn"
+              type="submit"
+              onClick={() => {
+                props.handleSelectProduct(props.product);
+                return navigate(`/productDetail/${props.product.id}`);
+              }}
+            >
+              Product Details
+            </button>
+            <button
+              className="trans-btn"
+              type="submit"
+              onClick={() => props.addProductToCart(props.product)}
+            >
+              Add To Cart
+            </button>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default ProductList;
